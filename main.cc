f6f319709de63d19984b2887fd2277e7830c4860
greenwookez <greenwookez@gmail.com>
2020-04-16 00:29:28 +0300

    Added multi-param templates by m-te

--- main.cc
+++ main.cc
@@ -1,89 +1,130 @@
 #include <iostream>
 #include <stdint.h>
 #include <assert.h>
-#include <windows.h>
 #include "sim.h"
 
 using namespace std;
-
-typedef uint32_t PageNumber; /* номер страницы */
 
 // ссылка на единственный объект симулятора
 Sim *g_pSim;
 
 // мой класс агента наследует базовый класс
-class Process : public Agent {
-    public:
+class MyAgent : public Agent
+{
+public:
+    // конструктор, деструктор
+    MyAgent() {}
+    ~MyAgent() {}
 
+    // метод, выполняющий некоторую деятельность
+    // в данном примере - выводящий строку в журнал
+    void KuKu(uint32_t n);
+    void Ku();
+    void KuStr(std::string str, int k);
 
-    void Work(uint32_t value) {
-        Log("Я работаю");
-    };
-
-    void MemoryRequest(PageNumber address, bool WriteFlag) {
-        Log("Я обращаюсь к памяти.");
-    };
-
-
-
-    void Start() {    
-        Schedule(1500, this, &Process::Work, 0);
-    };
+    // метод запуска агента
+    // (переопределение виртуального метода базового класса)
+    void Start();
 };
 
+// реализация метода, выполняющего некоторую деятельность
+void MyAgent::KuKu(uint32_t n)
+{
+    // вывод строки
+    Log("Ку-ку");
 
-class MyAgent2 : public Agent {
-    public:
-    void DoSomething(uint32_t n) {
-        Log("Doing something...");
+    // если не истекло заданное количество срабатываний
+    if(n>0)
+    {
+        // то планируем еще один вызов этого метода через 1500 нс,
+        // а также уменьшаем счетчик срабатываний
+        Schedule(GetTime()+1500, this, &MyAgent::KuKu,n-1);
+    }
+}
 
-        if (n>0) {
-            Schedule(GetTime() + 500,this, &MyAgent2::DoSomething,n-1);
-        }
-    };
+// реализация метода, выполняющего некоторую деятельность
+void MyAgent::Ku()
+{
+    // вывод строки
+    Log("Ку!");
+}
 
-    void Start() {
-        Schedule(0, this, &MyAgent2::DoSomething, 5);
-    };
-};
+void MyAgent::KuStr(std::string str, int val)
+{
+    Log(str);
 
+    if(val>0)
+    {
+        // то планируем еще один вызов этого метода через 1500 нс,
+        // а также уменьшаем счетчик срабатываний
+        Schedule(GetTime()+500, this, &MyAgent::KuStr,str,val-1);
+    }
+}
 
+// метод запуска моего агента
+// в этом методе необходимо создать как минимум одно событие
+void MyAgent::Start()
+{
+    // планирование события: в момент врмени 5000 нс для объекта this
+    // будет вызван метод KuKu и ему передано количество срабатываний 20
+    Schedule(5000, this, &MyAgent::KuKu, 100);
+}
 
+// главная функция программы, вызывается из оболочки
 int main()
 {
-    /* Для отображения русских символов в консоли на Windows */
-    SetConsoleCP(CP_UTF8);
-    SetConsoleOutputCP(CP_UTF8);
-
-
+    // создаем единственный объект симулятора
     g_pSim = new Sim;
 
-    Process *pProcess = new Process;
-    MyAgent2 *pAgent2 = new MyAgent2;
+    // создаем объект агента
+    MyAgent *pAgent = new MyAgent;
 
-    pProcess -> SetName("Проц");
-    pAgent2 -> SetName("Agent2");
-    pProcess -> Start();
-    pAgent2 -> Start();
-    
+    // устанавливаем имя агента (будет выводиться в журнал)
+    // желательно не более 9 символов
+    pAgent->SetName("Agent1");
 
+    // запуск агента
+//    pAgent->Start();
+    Schedule(5000, pAgent, &MyAgent::KuKu, 0);
+    Schedule(2000, pAgent, &MyAgent::KuKu, 0);
+    Schedule(25000, pAgent, &MyAgent::KuKu, 0);
+    Schedule(4000, pAgent, &MyAgent::Ku);
+    Schedule(2000, pAgent, &MyAgent::KuKu, 0);
+    Schedule(15000, pAgent, &MyAgent::KuKu, 0);
+    Schedule(26000, pAgent, &MyAgent::Ku);
+    Schedule(5000, pAgent, &MyAgent::KuKu, 0);
+    Schedule(5000, pAgent, &MyAgent::KuStr, "длинная строка", 50);
+
+    // предел времени симуляции 30000 нс
     g_pSim->SetLimit(30000);
 
-    g_pSim->Run();
-
-    PrintTime(&std::cout);
-    std::cout << " Do you want to proceed? [Y/n]" << std::endl;
-
-    char c[3];
-    std::cin.get(c,sizeof(c),'\n');
-    if(c[0] != 'n')
+    // запускаем симулятор. Он будет работать до тех пор, пока
+    // есть события в очереди и не достигнут заданный предел времени
+    while(!g_pSim->Run())
     {
-        g_pSim->SetLimit(60000);
-        g_pSim->Run();
+        // выводим запрос на продолжение работы симулятора
+        PrintTime(&std::cout);
+        std::cout << "Do you want to proceed? [Y/n]" << std::endl;
+
+        // вводим ответ пользователя
+        char c[3];
+        std::cin.getline(c,sizeof(c));
+
+        // если пользователь не против продолжения
+        if(c[0] == 'n')
+        {
+            break;
+        }
+
+        // устанавливаем новый предел времени и вновь запускаем симулятор
+        g_pSim->SetLimit(g_pSim->GetTime()+30000);
     }
 
-    delete pProcess;
-    delete pAgent2;
+    // удаляем объект агента
+    delete pAgent;
+
+    // удаляем объект симулятора
+    delete g_pSim;
 
     return 0;
 }
--- sim.h
+++ sim.h
@@ -29,7 +29,6 @@ public:
     virtual ~Event() {}
 
     SimulatorTime m_time;
-    uint32_t m_value;
     Event *m_pNext;
 
     virtual void Notify() {}
@@ -76,20 +75,20 @@ private:
 
 extern Sim *g_pSim;
 
+// обработчик без параметров
 template <typename METHOD, typename OBJECT>
-uint64_t Schedule(SimulatorTime time, OBJECT obj, METHOD met, uint32_t value)
+uint64_t Schedule(SimulatorTime time, OBJECT obj, METHOD met)
 {
     assert(time >= g_pSim->GetTime());
-    return g_pSim->Schedule(MakeEvent(time, obj, met, value));
-}
-
-template <typename METHOD, typename OBJECT>
-Event *MakeEvent(SimulatorTime time, OBJECT obj, METHOD met, uint32_t value)
-{
     class EventImpl : public Event
     {
     public:
-        EventImpl(SimulatorTime time, OBJECT obj, METHOD met, uint32_t value) { m_time=time; m_obj=obj; m_met=met; m_value=value; }
+        EventImpl(SimulatorTime time, OBJECT obj, METHOD met)
+        {
+            m_time = time;
+            m_obj = obj;
+            m_met = met;
+        }
         virtual ~EventImpl() {}
 
         OBJECT m_obj;
@@ -97,12 +96,139 @@ Event *MakeEvent(SimulatorTime time, OBJECT obj, METHOD met, uint32_t value)
 
         void Notify()
         {
-            (m_obj->*m_met)(m_value);
+            (m_obj->*m_met)();
         }
-    } *pEvent = new EventImpl(time, obj, met, value);
+    } *pEvent = new EventImpl(time, obj, met);
+    return g_pSim->Schedule(pEvent);
+}
 
-    return pEvent;
-};
+// обработчик с одним параметром
+template <typename METHOD, typename OBJECT, typename PARAM1>
+uint64_t Schedule(SimulatorTime time, OBJECT obj, METHOD met, PARAM1 value1)
+{
+    assert(time >= g_pSim->GetTime());
+    class EventImpl : public Event
+    {
+    public:
+        EventImpl(SimulatorTime time, OBJECT obj, METHOD met, PARAM1 value1)
+        {
+            m_time = time;
+            m_obj = obj;
+            m_met = met;
+            m_value1 = value1;
+        }
+        virtual ~EventImpl() {}
+
+        OBJECT m_obj;
+        METHOD m_met;
+        PARAM1 m_value1;
+
+        void Notify()
+        {
+            (m_obj->*m_met)(m_value1);
+        }
+    } *pEvent = new EventImpl(time, obj, met, value1);
+    return g_pSim->Schedule(pEvent);
+}
+
+// обработчик с двумя параметрами
+template <typename METHOD, typename OBJECT, typename PARAM1, typename PARAM2>
+uint64_t Schedule(SimulatorTime time, OBJECT obj, METHOD met, PARAM1 value1, PARAM2 value2)
+{
+    assert(time >= g_pSim->GetTime());
+    class EventImpl : public Event
+    {
+    public:
+        EventImpl(SimulatorTime time, OBJECT obj, METHOD met, PARAM1 value1, PARAM2 value2)
+        {
+            m_time = time;
+            m_obj = obj;
+            m_met = met;
+            m_value1 = value1;
+            m_value2 = value2;
+        }
+        virtual ~EventImpl() {}
+
+        OBJECT m_obj;
+        METHOD m_met;
+        PARAM1 m_value1;
+        PARAM2 m_value2;
+
+        void Notify()
+        {
+            (m_obj->*m_met)(m_value1, m_value2);
+        }
+    } *pEvent = new EventImpl(time, obj, met, value1, value2);
+    return g_pSim->Schedule(pEvent);
+}
+
+// обработчик с тремя параметрами
+template <typename METHOD, typename OBJECT, typename PARAM1, typename PARAM2, typename PARAM3>
+uint64_t Schedule(SimulatorTime time, OBJECT obj, METHOD met, PARAM1 value1, PARAM2 value2, PARAM3 value3)
+{
+    assert(time >= g_pSim->GetTime());
+    class EventImpl : public Event
+    {
+    public:
+        EventImpl(SimulatorTime time, OBJECT obj, METHOD met, PARAM1 value1, PARAM2 value2, PARAM3 value3)
+        {
+            m_time = time;
+            m_obj = obj;
+            m_met = met;
+            m_value1 = value1;
+            m_value2 = value2;
+            m_value3 = value3;
+        }
+        virtual ~EventImpl() {}
+
+        OBJECT m_obj;
+        METHOD m_met;
+        PARAM1 m_value1;
+        PARAM2 m_value2;
+        PARAM3 m_value3;
+
+        void Notify()
+        {
+            (m_obj->*m_met)(m_value1, m_value2, m_value3);
+        }
+    } *pEvent = new EventImpl(time, obj, met, value1, value2, value3);
+    return g_pSim->Schedule(pEvent);
+}
+
+// обработчик с четырьмя параметрами
+template <typename METHOD, typename OBJECT, typename PARAM1, typename PARAM2, typename PARAM3, typename PARAM4>
+uint64_t Schedule(SimulatorTime time, OBJECT obj, METHOD met, PARAM1 value1, PARAM2 value2, PARAM3 value3, PARAM4 value4)
+{
+    assert(time >= g_pSim->GetTime());
+    class EventImpl : public Event
+    {
+    public:
+        EventImpl(SimulatorTime time, OBJECT obj, METHOD met, PARAM1 value1, PARAM2 value2, PARAM3 value3, PARAM4 value4)
+        {
+            m_time = time;
+            m_obj = obj;
+            m_met = met;
+            m_value1 = value1;
+            m_value2 = value2;
+            m_value3 = value3;
+            m_value4 = value4;
+        }
+        virtual ~EventImpl() {}
+
+        OBJECT m_obj;
+        METHOD m_met;
+        PARAM1 m_value1;
+        PARAM2 m_value2;
+        PARAM3 m_value3;
+        PARAM4 m_value4;
+
+        void Notify()
+        {
+            (m_obj->*m_met)(m_value1, m_value2, m_value3, m_value4);
+        }
+    } *pEvent = new EventImpl(time, obj, met, value1, value2, value3, value4);
+    return g_pSim->Schedule(pEvent);
+}
 
 bool CancelEvent(uint64_t handle);
 
